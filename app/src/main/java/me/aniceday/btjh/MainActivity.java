680b3bab71cf48163581dfef1d2e5907cf5a0dba
pbbqdd <pbbqdd@users.noreply.github.com>
2019-10-07 22:11:44 +0800

    反激活
    添加反激活

--- app/src/main/java/me/aniceday/btjh/MainActivity.java
+++ app/src/main/java/me/aniceday/btjh/MainActivity.java
@@ -1,49 +1,112 @@
 package me.aniceday.btjh;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
-import java.io.DataInput;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 //import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
     private String[] d = {"export CLASSPATH=/data/local/tmp/.knife/inject.jar\n","exec app_process /data/local/tmp/.knife com.coffee.injectmotionevent.main.InjectMotionEvent 0 200 300 &\n","exec /data/local/tmp/knife_server &\n"};
-
-    private void a(String paramString) {
+    private String[] s = {"kill -9 $(pidof com.zuoyou.inject)\n","kill -9 $(pidof knife_server)\n"};
+    private void cmd(String paramString, String[] cmdString) {
         try {
             Process localProcess = Runtime.getRuntime().exec(paramString);
             DataOutputStream param = new DataOutputStream(localProcess.getOutputStream());
-            for (int i = 0; i < d.length; i++) {
-                param.writeBytes(d[i].toString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(localProcess.getInputStream()));
+            for (int i = 0; i < cmdString.length; i++) {
+                param.writeBytes(cmdString[i].toString());
             }
             param.flush();
-            localProcess.waitFor();
+//            localProcess.waitFor();
+
         } catch (Exception p) {
             p.printStackTrace();
         }
     }
 
+
+
+    public Boolean check_server() {
+        try {
+            Process localProcess = Runtime.getRuntime().exec("su -c pidof com.zuoyou.inject");
+            BufferedReader in = new BufferedReader(new InputStreamReader(localProcess.getInputStream()));
+            String s;
+            if(in.readLine()==null){
+                return false;
+            }else{
+                return true;
+            }
+
+
+
+        } catch (Exception p) {
+            p.printStackTrace();
+            return false;
+        }
+    }
+    private void buttenStats(Boolean s,FloatingActionButton fab){
+        if(s){
+            fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
+            fab.setImageResource(android.R.drawable.ic_media_pause);
+
+
+        }else {
+            fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
+            fab.setImageResource(android.R.drawable.ic_media_play);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
+//        TextView text = findViewById(R.id.showtext);
+//        text.setText(check_server().toString());
+        this.buttenStats(check_server(),fab);
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "已运行", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                a("su");
+                switch (check_server().toString()){
+                    case "false":
+                        fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
+                        fab.setImageResource(android.R.drawable.ic_media_pause);
+//                        Snackbar.make(view, "已运行", Snackbar.LENGTH_LONG)
+//                                .setAction("Action", null).show();
+                        cmd("su",d);
+                    break;
+                    case "true":
+                        fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
+                        fab.setImageResource(android.R.drawable.ic_media_play);
+//                        Snackbar.make(view, "", Snackbar.LENGTH_LONG)
+//                                .setAction("Action", null).show();
+                        cmd("su",s);
+
+                }
+
+
             }
         });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+
     }
 }

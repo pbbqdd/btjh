fb8de2435d31bc935d524dc2a648ac4a41b9724b
pbbqdd <pbbqdd@users.noreply.github.com>
2019-10-11 18:57:12 +0800

    AD

--- app/src/main/java/me/aniceday/btjh/MainActivity.java
+++ app/src/main/java/me/aniceday/btjh/MainActivity.java
@@ -3,13 +3,21 @@ package me.aniceday.btjh;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -18,6 +26,7 @@ import java.io.InputStreamReader;
 //import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
     private String[] d = {"export CLASSPATH=/data/local/tmp/.knife/inject.jar\n","exec app_process /data/local/tmp/.knife com.coffee.injectmotionevent.main.InjectMotionEvent 0 200 300 &\n","exec /data/local/tmp/knife_server &\n"};
     private String[] s = {"kill -9 $(pidof com.zuoyou.inject)\n","kill -9 $(pidof knife_server)\n"};
     private void cmd(String paramString, String[] cmdString) {
@@ -74,35 +83,92 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-//        TextView text = findViewById(R.id.showtext);
-//        text.setText(check_server().toString());
-        this.buttenStats(check_server(),fab);
+        buttenStats(check_server(),fab);
 
 
-
-        fab.setOnClickListener(new View.OnClickListener() {
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
-            public void onClick(View view) {
-                switch (check_server().toString()){
-                    case "false":
-                        fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
-                        fab.setImageResource(android.R.drawable.ic_media_pause);
-//                        Snackbar.make(view, "已运行", Snackbar.LENGTH_LONG)
-//                                .setAction("Action", null).show();
-                        cmd("su",d);
-                    break;
-                    case "true":
-                        fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
-                        fab.setImageResource(android.R.drawable.ic_media_play);
-//                        Snackbar.make(view, "", Snackbar.LENGTH_LONG)
-//                                .setAction("Action", null).show();
-                        cmd("su",s);
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        switch (check_server().toString()){
+                            case "false":
+                                fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
+                                fab.setImageResource(android.R.drawable.ic_media_pause);
+                                cmd("su",d);
+                                break;
+                            case "true":
+                                fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
+                                fab.setImageResource(android.R.drawable.ic_media_play);
+                                cmd("su",s);
 
-                }
+                        }
+
+
+                    }
+                });
+                // Code to be executed when an ad finishes loading.
+//
 
 
             }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+//                fab.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        switch (check_server().toString()){
+//                            case "false":
+//                                fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
+//                                fab.setImageResource(android.R.drawable.ic_media_pause);
+//                                cmd("su",d);
+//                                break;
+//                            case "true":
+//                                fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
+//                                fab.setImageResource(android.R.drawable.ic_media_play);
+//                                cmd("su",s);
+//
+//                        }
+//
+//
+//                    }
+//                });
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
         });
+
     }
     @Override
     protected void onStart(){

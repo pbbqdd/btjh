f058fe4cce4f61af025033eac557bd65acf07a45
pbbqdd <pbbqdd@users.noreply.github.com>
2019-10-16 17:48:46 +0800

    -
     -

--- app/src/main/java/me/ioiu/btjh/MainActivity.java
+++ app/src/main/java/me/ioiu/btjh/MainActivity.java
@@ -1,27 +1,19 @@
-package me.aniceday.btjh;
+package me.ioiu.btjh;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
-
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 //import android.view.Menu;
 
@@ -94,78 +86,24 @@ public class MainActivity extends AppCompatActivity {
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
-        mAdView.setAdListener(new AdListener() {
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAdLoaded() {
-                fab.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        switch (check_server().toString()){
-                            case "false":
-                                fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
-                                fab.setImageResource(android.R.drawable.ic_media_pause);
-                                cmd("su",d);
-                                break;
-                            case "true":
-                                fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
-                                fab.setImageResource(android.R.drawable.ic_media_play);
-                                cmd("su",s);
+            public void onClick(View view) {
+                switch (check_server().toString()){
+                    case "false":
+                        fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
+                        fab.setImageResource(android.R.drawable.ic_media_pause);
+                        cmd("su",d);
+                        break;
+                    case "true":
+                        fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
+                        fab.setImageResource(android.R.drawable.ic_media_play);
+                        cmd("su",s);
 
-                        }
+                }
 
 
-                    }
-                });
-                // Code to be executed when an ad finishes loading.
-//
-
-
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                // Code to be executed when an ad request fails.
-//                fab.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        switch (check_server().toString()){
-//                            case "false":
-//                                fab.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("red")));
-//                                fab.setImageResource(android.R.drawable.ic_media_pause);
-//                                cmd("su",d);
-//                                break;
-//                            case "true":
-//                                fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_blue_light)));
-//                                fab.setImageResource(android.R.drawable.ic_media_play);
-//                                cmd("su",s);
-//
-//                        }
-//
-//
-//                    }
-//                });
-            }
-
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-            }
-
-            @Override
-            public void onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when the user is about to return
-                // to the app after tapping on an ad.
             }
         });
 

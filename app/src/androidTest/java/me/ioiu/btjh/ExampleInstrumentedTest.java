41e49cc0e8d8ddd7e75b0dbe57e5e6ae244a69f8
pbbqdd <pbbqdd@users.noreply.github.com>
2019-10-20 17:35:34 +0800

    -

--- app/src/androidTest/java/me/ioiu/btjh/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/me/ioiu/btjh/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("me.aniceday.btjh", appContext.getPackageName());
+        assertEquals("me.ioiu.btjh", appContext.getPackageName());
     }
 }
